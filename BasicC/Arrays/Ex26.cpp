5845cb45e250bb348c23ef3ed4994d81e0b0f9a6
Matan Oded <matanoded@gmail.com>
2018-09-05 16:39:58 +0300

    Day2-3

---
+++ BasicC/Arrays/Ex26.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define n 9
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void main()
+{
+	// Varriables
+	int series[n] {2,3,4,2,3,4,5,6,7};
+	int space;
+	bool isSerialized = true;
+
+	// Code
+	// Find how many indexes between each predicted series
+	for (int i = 1; i < n; i++)
+	{
+		if (series[i] == series[0])
+		{
+			space = i;
+			break;
+		}
+	}
+	
+	// For every index, check if it's following one between each space is equal to him
+	for (int i = 0; i+space < n; i+=space)
+	{
+		for (int j = i; j < i+space; j++)
+		{
+			if (series[j] != (series[j + space]))
+				isSerialized = false;
+		}
+		if (!isSerialized)
+		{
+			break;
+		}
+	}
+
+	// Print the correct massage
+	if (isSerialized)
+	{
+		printf ("It is serialized");
+	}
+	else
+	{
+		printf ("It's not serialized");
+	}
+	
+	// Please stop so I can see what happend
+	scanf ("%d");
+}
--- BasicC/Loops/ExC.cpp
+++ BasicC/Loops/ExC.cpp
@@ -6,13 +6,30 @@
 void main()
 {
 	// Varriables
-	int n,k;
+	int n,k,mone=0;
 
 	// Code
-	printf ("Enter number and parts");
+	printf ("Enter number and parts\n");
 	scanf ("%d%d",&n,&k);
 
-	for 
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			for (int l = i; l < n; l++)
+			{
+				printf ("%d",l);
+				mone++;
+			}
+			for (int l = 0; l < n-mone; l++)
+			{
+				printf ("%d",l);
+			}
+			printf ("@");
+			mone = 0;
+		}
+		printf ("\n");
+	}
 
 	// Please stop so I can see what happend
 	scanf ("%d");
