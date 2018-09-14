e1a93ea4c2cc8104e3d38a9b7ba03a9a30a0a132
Matan Oded <matanoded@gmail.com>
2018-09-14 19:24:09 +0300

    omg more strings

--- BasicC/Strings/Exercises.cpp
+++ BasicC/Strings/Exercises.cpp
@@ -1,5 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define n 10
+#define n 256
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -19,8 +19,7 @@ void ex1()
 	_strrev(str);
 	printf("%s\n ", str);
 
-	// Please stop so I can see what happend
-	scanf ("%d");
+	
 }
 
 void ex2()
@@ -48,12 +47,115 @@ void ex2()
 
 	//d
 	strcpy(str, original);
+	int i=0;
+	while (str[i]!=NULL)
+	{
+		printf("%c", str[i]);
+		printf("%c", str[i]);
+		i++;
+	}
+}
 
-	// Please stop so I can see what happend
-	scanf("%d");
+void ex3()
+{
+	// Varriables
+	int i=0, j=0;
+	String str1, str2, finalPrint;
+	printf("Enter 2 strings\n");
+	scanf("%s %s", str1, str2);
+
+	while (str1[i] != NULL)
+	{
+		if (strchr(str2, str1[i]) > 0)
+		{
+			finalPrint[j] = str1[i];
+			j++;
+		}
+		i++;
+	}
+	printf("%s", finalPrint);
+
+	
+}
+
+// ??
+//void ex4()
+//{
+//	String str;
+//	char *temp;
+//	int i = 0;
+//	printf("Enter string\n");
+//	scanf("%s", str);
+//	while (str[i] != NULL)
+//	{
+//		*temp = str[i];
+//		for (int j = 0; j < strstr(str, temp); j++)
+//		{
+//
+//		}
+//	}
+//}
+
+void ex5()
+{
+	String original, str, helper;
+	int N;
+	int i = 0, k;
+	printf("Enter string and number of loops\n");
+	scanf("%s", original);
+	strcpy(str, original);
+	scanf("%d", &N);
+	for (int j = 0; j < N; j++)
+	{
+		i = 0;
+		while (str[i+1] != NULL)
+		{
+			i++;
+		}
+		helper[0] = str[i];
+		for (k = 1; k <= i; k++)
+		{
+			helper[k] = str[k-1];
+		}
+		helper[k] = NULL;
+		strcpy(str, helper);
+	}
+	printf("%s\n", str);
+	k = strcmp(original, str);
+	if (k == 0)
+	{
+		printf("Equal");
+	}
+	else
+	{
+		printf("Different: %d" , k);
+	}
+}
+
+void ex6()
+{
+	String words[10];
+	for (int i = 0; i < 100; i++)
+	{
+		printf("Enter word\n");
+		scanf("%s", words[i]);
+	}
+	for (int i = 0; i < 100; i++)
+	{
+		printf("%s.\n" , words[i]);
+	}
+
+}
+
+void ex7()
+{
+
 }
 
 void main()
 {
-	ex1();
+	ex6();
+
+	// Please stop so I can see what happend
+	scanf("%d"); scanf("%d");
 }
