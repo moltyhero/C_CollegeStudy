cfb9ec257b3d85b9241e9295967f35b4a4ded0f1
Matan Oded <matanoded@gmail.com>
2018-10-09 11:31:14 +0300

    .

--- BasicC/Files/FilesSeek.cpp
+++ BasicC/Files/FilesSeek.cpp
@@ -11,9 +11,9 @@ typedef struct
 	int mark;
 } student;
 
-int main()
+int main2()
 {
-	int x;
+	//int x;
 	student arr[3] = { {"Yosi", 85}, {"Moshe", 90}, {"Yona", 60} };
 	student stu;
 
@@ -27,7 +27,50 @@ int main()
 	fwrite(arr, sizeof(student), 3, fp);
 	fclose(fp);
 
+	
+	return 0;
+}
+
+int main()
+{
+	//int x;
+	student arr[3] = { {"Yosi", 85}, {"Moshe", 90}, {"Yona", 60} };
+	student stu;
+
+	FILE *fp;
+
+	// Change moshe grade
 	fp = fopen("FileDAT.DAT", "r+b");
+	fwrite(arr, sizeof(student), 3, fp);
+	fclose(fp);
+
+	fp = fopen("FileDAT.DAT", "r+b");
+
+	fread(&stu, sizeof(student), 1, fp);
+	int b = 0;
+	while (!feof(fp) && b != 1)
+	{
+		if (!strcmp("Moshe", stu.name))
+		{
+			//printf("11\n");
+			stu.mark = 95;
+			fseek(fp, -(long)sizeof(student), SEEK_CUR);
+			fwrite(&stu, sizeof(student), 1, fp);
+			//fseek(fp, 0, SEEK_CUR); // Should be useless?
+			b = 1;
+		}
+		fread(&stu, sizeof(student), 1, fp);
+	}
+	fclose(fp);
+	fp = fopen("FILEDAT.DAT", "rb");
+	fread(arr, sizeof(student), 3, fp);
+	fclose(fp);
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%s %d\n", arr[i].name, arr[i].mark);
+	}
+
+	scanf("%d");
 
 	return 0;
 }
